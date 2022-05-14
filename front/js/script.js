2320843a8585f9fb56958ad0758a5b93f8b8f5d6
tehami soumia <sou_mia_46@hotmail.fr>
2022-05-14 18:10:59 +0200

    func addproducts

--- front/js/script.js
+++ front/js/script.js
@@ -1,11 +1,16 @@
 fetch("http://localhost:3000/api/products")
 .then(res=> res.json())
-.then(data=> console.log(data))
+.then(data=> addproducts(data))
 
+function addproducts(donnes){
+const  imageUrl=donnes[0]. imageUrl
 let hr=document.createElement('a');
 hr.href= "http://localhost:3000/images/kanap01.jpeg"
 hr.text="canape"
-const items=document.querySelector("items")
+const items=document.querySelector("#items")
 if (items !=null){
    items.appendChild(hr)
-}
+}}
+
+
+
