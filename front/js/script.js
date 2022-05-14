fcd4f4f985e88e41a961ea5b57bb65a414bc3438
tehami soumia <sou_mia_46@hotmail.fr>
2022-05-14 16:31:47 +0200

    fetch

--- front/js/script.js
+++ front/js/script.js
@@ -1,4 +1,11 @@
-fetch('https://http//localhost:3000/back/product')
-     .then(res=> res.json())
-     .then(data=> console.log(data))
-     console.log()
+fetch("http://localhost:3000/api/products")
+.then(res=> res.json())
+.then(data=> console.log(data))
+
+let hr=document.createElement('a');
+hr.href= "http://localhost:3000/images/kanap01.jpeg"
+hr.text="canape"
+const items=document.querySelector("items")
+if (items !=null){
+   items.appendChild(hr)
+}
