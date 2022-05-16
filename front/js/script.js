fca0fd31a6d4cfe38e6099b1478c81854f971d02
tehami soumia <sou_mia_46@hotmail.fr>
2022-05-16 06:11:29 +0200

    href

--- front/js/script.js
+++ front/js/script.js
@@ -1,16 +1,25 @@
 fetch("http://localhost:3000/api/products")
 .then(res=> res.json())
-.then(data=> addproducts(data))
+.then(data=> {
+    return addproducts(data)})
 
-function addproducts(donnes){
-const  imageUrl=donnes[0]. imageUrl
-let hr=document.createElement('a');
-hr.href= "http://localhost:3000/images/kanap01.jpeg"
-hr.text="canape"
-const items=document.querySelector("#items")
-if (items !=null){
-   items.appendChild(hr)
-}}
+function addproducts(data){
+const _id=data[0]._id
+const hr=  makehr( _id)
+appendElementtohr(hr)
 
+} 
 
+    function makehr(_id){
+        const hr=document.createElement('a');
+        hr.href= "./product.html?id="+_id
+        return hr
+    }
+    function appendElementtohr(hr){
+        const items=document.querySelector("#items")
+        if (items !=null){
+        items.appendChild(hr)
+    
+        }
 
+    }
