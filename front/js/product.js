7dc86aa54520a7a166ce8785e5c9ba643c041016
tehami soumia <sou_mia_46@hotmail.fr>
2022-05-19 00:00:30 +0200

    fetch id

--- front/js/product.js
+++ front/js/product.js
@@ -2,5 +2,11 @@
 const queryString = window.location.search;
 const Params = new URLSearchParams(queryString);
 // la variable id va récupérer la valeur du paramètre _id
-const Id = Params.get("id");
-console.log({Id}); 
+const id = Params.get("id");
+console.log({id}); 
+function init() {
+    fetch("http://localhost:3000/api/products/"+id)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }
+  init();
