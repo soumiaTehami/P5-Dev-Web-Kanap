919e343a9e5b2388d258f6b3d1f06964b5cfbe62
tehami soumia <sou_mia_46@hotmail.fr>
2022-05-20 16:00:30 +0200

    ajouté img produits

--- front/js/product.js
+++ front/js/product.js
@@ -1,12 +1,26 @@
+function init() {
 //la variable params récupère l'url de la page
 const queryString = window.location.search;
 const Params = new URLSearchParams(queryString);
 // la variable id va récupérer la valeur du paramètre _id
 const id = Params.get("id");
 console.log({id}); 
-function init() {
     fetch("http://localhost:3000/api/products/"+id)
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => lesProduits(data));
   }
   init();
+  function lesProduits(produit) {
+    const imageUrl = produit.imageUrl;
+    const altTxt = produit.altTxt;
+    makeImage(imageUrl, altTxt);
+  }
+  function makeImage(imageUrl, altTxt) {
+    const image = document.createElement("img");
+    image.src = imageUrl;
+    image.alt = altTxt;
+    const parent = document.querySelector(".item__img");
+    if (parent != null) {
+      parent.appendChild(image);
+  }
+}
