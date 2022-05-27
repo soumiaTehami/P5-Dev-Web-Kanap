d8d4072fc58af403999d92c524e45809353da0ff
tehami soumia <sou_mia_46@hotmail.fr>
2022-05-27 10:38:24 +0200

    image panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,5 +1,6 @@
 const cart=[]
 affichagePanier()
+cart.forEach((item) => affiche(item))
 function affichagePanier() {
 const numbItem=localStorage.length
 for (let i = 0; i < numbItem ; i++) {
@@ -9,3 +10,35 @@ for (let i = 0; i < numbItem ; i++) {
  }
  console.log(cart)
 }
+function affiche(item){
+const article = makeArticle(item);
+const div = makeImage(item);
+appendArticle(article)
+article.appendChild(div)
+
+console.log(article)
+
+}
+function appendArticle(article){
+    document.querySelector("#cart__items").appendChild(article)
+    
+    
+}
+function makeArticle(item){
+ const article = document.createElement("article");
+ article.classList.add("cart__item")
+ article.dataset.id=item.id
+ article.dataset.color=item.color 
+ 
+ return article
+}
+
+function makeImage(item) {
+    const div = document.createElement("div"); 
+    div.classList.add("cart__item__img")
+    const image = document.createElement("img");
+    image.src = item.imageUrl;
+    image.alt = item.altTxt;
+    div.appendChild(image)
+    return div;
+  }
